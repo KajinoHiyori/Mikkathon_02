3228761f6b3060d6b541502939c3a62bcf829260
KajinoHiyori <251312@st.yoshida-g.ac.jp>
2026-03-26 12:08:26 +0900

    距離表示処理追加

---
+++ distance.h
@@ -0,0 +1,29 @@
+//========================================================
+// 
+// ケプラーMk3 [fade.h]
+// Author : Kajino Hiyori
+// 
+//========================================================
+#if 0
+#ifndef _FADE_H_
+#define	_FADE_H_
+#include "main.h"
+
+// フェードの状態
+typedef enum
+{
+	FADE_NONE = 0,	// 何もしていない状態
+	FADE_IN,		// フェードイン
+	FADE_OUT,		// フェードアウト
+	FADE_MAX
+}FADE;
+
+// プロトタイプ宣言
+void InitFade(MODE modeNext, D3DXCOLOR col);
+void UninitFade(void);
+void UpdateFade(void);
+void DrawFade(void);
+void SetFade(MODE modeNext, D3DXCOLOR col);
+FADE GetFade(void);
+#endif
+#endif
